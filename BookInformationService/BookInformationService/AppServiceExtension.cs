d9ffb229d9688348efec6b07f4d885bebd6c45d9
Ahmad <engzaman2020@gmail.com>
2024-07-25 23:09:05 +0300

    Added project "Build .NET Minimal API Service with PostgreSQL using Docker Compose"

---
+++ BookInformationService/BookInformationService/AppServiceExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookInformationService.DatabaseContext;
+
+namespace BookInformationService;
+
+public static class AppServiceExtension
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope serviceScope = app.ApplicationServices.CreateScope();
+
+        using SystemDbContext dbContext = serviceScope.ServiceProvider.GetRequiredService<SystemDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+
+}
+
---
+++ BookInformationService/BookInformationService/BookInformation/Facade/Get/GetBookInformationDL.cs
@@ -0,0 +1,42 @@
+﻿using BookInformationService.DatabaseContext;
+using BookInformationService.Util;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookInformationService.BookInformation.Facade.Get;
+
+public class GetBookInformationDL : IGetBookInformationDL
+{
+    private readonly ILogger<object> _logger;
+    private readonly SystemDbContext _dbContext;
+
+    public GetBookInformationDL(ILogger<object> logger, SystemDbContext dbContext)
+    {
+        _logger = logger;
+        _dbContext = dbContext;
+    }
+
+    public async Task<Dictionary<string, object?>> GetBookInformation(int id)
+    {
+        try
+        {
+            BookInformationModel? bookInformation = await _dbContext.DbSetBookInformation.FindAsync(id);
+
+            return new Dictionary<string, object?>
+            {
+                { "Message", string.Empty },
+                { "BookInformation", bookInformation }
+            };
+        }
+        catch (Exception ex)
+        {
+            ex.ErrorLog(_logger);
+
+            return new Dictionary<string, object?>
+            {
+                { "Message", ex.Message },
+                { "BookInformation", null }
+            };
+        }
+    }
+}
+
